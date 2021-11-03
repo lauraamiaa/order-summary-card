df0a16606d31d5aa4543cc1907b24a2b54b8123d
lauraamiaa <miatkowski.laura@gmail.com>
2021-11-03 22:01:18 +0100

    implemented text

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <div>
+        <h1>Order Summary</h1>
+        <p>
+          You can now listen to millions of songs, audiobooks, and podcasts on
+          any device anywhere you like!
+        </p>
+      </div>
+      <div>
+        <h4>Annual Plan</h4>
+        <p>$59.99/year</p>
+        <a>Change</a>
+      </div>
+      <button>Proceed to Payment</button>
+      <a>Cancel Order</a>
+    </div>
+  );
 }
 
 export default App;
